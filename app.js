f55c0965369f8cddd376f7833bb9fd2c13322cdd
Arasy Yoram <yoram@Lian.local>
2021-08-22 23:55:46 +0700

    add start script

--- app.js
+++ app.js
@@ -170,6 +170,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-app.listen(3000, () => {
-    console.log('listening...')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening ${port}...`)
 })
